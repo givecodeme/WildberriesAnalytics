6e088d9120e12dc3b6c9f6b140d4ad040d738165
Danil <daniil07042001@mail.ru>
2021-01-23 21:37:08 +0300

    test

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="hello">
-    <img src='@/assets/logo-django.png' style="width: 250px" />
-    <p>The data below is added/removed from the SQLite Database using Django's ORM and Rest Framework.</p>
-    <br/>
+    <img src="@/assets/logo-django.png" style="width: 250px" />
+    <p>
+      The data below is added/removed from the SQLite Database using Django's
+      ORM and Rest Framework.
+    </p>
+    <br />
     <p>Subject</p>
-    <input type="text" placeholder="Hello" v-model="subject">
+    <input type="text" placeholder="Hello" v-model="subject" />
     <p>Message</p>
-    <input type="text" placeholder="From the other side" v-model="msgBody">
-    <br><br>
-    <input 
-      type="submit" 
-      value="Add" 
-      @click="addMessage({ subject: subject, body: msgBody })" 
-      :disabled="!subject || !msgBody">
+    <input type="text" placeholder="From the other side" v-model="msgBody" />
+    <br /><br />
+    <input
+      type="submit"
+      value="Add"
+      @click="addMessage({ subject: subject, body: msgBody })"
+      :disabled="!subject || !msgBody"
+    />
 
-    <hr/>
+    <hr />
     <h3>Messages on Database</h3>
     <p v-if="messages.length === 0">No Messages</p>
     <div class="msg" v-for="(msg, index) in messages" :key="index">
-        <p class="msg-index">[{{index}}]</p>
+      <b-card>
+        <p class="msg-index">[{{ index }}]</p>
         <p class="msg-subject" v-html="msg.subject"></p>
         <p class="msg-body" v-html="msg.body"></p>
         <input type="submit" @click="deleteMessage(msg.pk)" value="Delete" />
+      </b-card>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "Messages",
@@ -38,15 +44,12 @@ export default {
     };
   },
   computed: mapState({
-    messages: state => state.messages.messages
+    messages: (state) => state.messages.messages,
   }),
-  methods: mapActions('messages', [
-    'addMessage',
-    'deleteMessage'
-  ]),
+  methods: mapActions("messages", ["addMessage", "deleteMessage"]),
   created() {
-    this.$store.dispatch('messages/getMessages')
-  }
+    this.$store.dispatch("messages/getMessages");
+  },
 };
 </script>
 
@@ -75,5 +78,4 @@ img {
   padding-top: 50px;
   padding-bottom: 50px;
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 
-import store from '@/store' 
+import store from '@/store'
 import router from '@/router'
 
-Vue.config.productionTip = false
 
-// Vue.use(VueRouter)
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue, IconsPlugin)
+
+
+Vue.config.productionTip = false
 
 const vue = new Vue({
   router,
